d48ca1ef9a9d8776ebf73e917f1bb01fe87a5433
穗乃果 <1763575579@qq.com>
2023-03-06 08:49:46 +0800

    Site updated: 2023-03-06 08:49:35

--- custom/main.js
+++ custom/main.js
@@ -3,7 +3,7 @@ window.onload = function () {
         Swal.fire({
             icon: 'warning',
             title: '广告被屏蔽',
-            html: '救救孩子吧，检测到您使用了广告屏蔽插件，建议您把广告屏蔽插件关闭或者添加本站为白名单即可。<br/><br/>你可以直接关闭本弹窗，假装没看见，没有任何影响(。・∀・)ノ',
+            text: '广告是维持小助手网站正常运转的唯一收入来源，检测到您使用了广告屏蔽插件，建议您把广告屏蔽插件关闭或者添加本站为白名单，感谢支持',
             showDenyButton: true,
             confirmButtonText: '😋 下次一定',
             confirmButtonColor: '#EBAA2A',
