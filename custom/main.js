4906d4302e6d76f2ebdb02546bc4a889d3151db5
穗乃果 <1763575579@qq.com>
2023-07-06 00:32:47 +0800

    Site updated: 2023-07-06 00:32:37

--- custom/main.js
+++ custom/main.js
@@ -3,15 +3,15 @@ window.onload = function () {
         Swal.fire({
             icon: 'warning',
             title: '广告被屏蔽',
-            text: '广告是维持小助手网站正常运转的唯一收入来源，检测到您使用了广告屏蔽插件，建议您把广告屏蔽插件关闭或者添加本站为白名单，感谢支持',
+            text: '谷歌广告是维持小助手网站正常运转的唯一收入来源，检测到您使用了广告屏蔽插件，建议您把广告屏蔽插件关闭或者添加本站为白名单，感谢你的支持',
             showDenyButton: true,
-            confirmButtonText: '😋 下次一定',
+            confirmButtonText: '😭 下次一定',
             confirmButtonColor: '#EBAA2A',
             denyButtonText: '😘 点我了解如何解除广告屏蔽',
             denyButtonColor: '#0984E3',
         }).then((result) => {
             if (result.isDenied) {
-                window.location.assign("https://crazyzhang.cn/gta5/goog.html");
+                window.location.assign("https://crazyzhang.cn/gta5/googleads.html");
             }
         })
     }
