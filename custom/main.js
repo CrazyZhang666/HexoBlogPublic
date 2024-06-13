e00295970947dd33ff6c10326f9aca7fe77327a0
穗乃果 <1763575579@qq.com>
2024-06-14 07:12:04 +0800

    Site updated: 2024-06-14 07:11:54

--- custom/main.js
+++ custom/main.js
@@ -3,7 +3,7 @@ window.onload = function () {
         Swal.fire({
             icon: 'warning',
             title: '广告被屏蔽',
-            text: '谷歌广告是维持小助手网站正常运转的唯一收入来源，检测到您使用了广告屏蔽插件，建议您把广告屏蔽插件关闭或者添加本站为白名单，感谢你的支持',
+            text: '检测到您使用了广告屏蔽插件，建议您把广告屏蔽插件关闭或者添加本站为白名单，感谢你的支持',
             showDenyButton: true,
             confirmButtonText: '😭 下次一定',
             confirmButtonColor: '#EBAA2A',
